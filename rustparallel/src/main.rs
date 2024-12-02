f0981f6f9eca1c59958c2426be34f49c8b7fa026
tunckoldas <tunc.koldas1@smail.th-koeln.de>
2024-12-02 15:50:28 +0100

    calculator

--- rustparallel/src/main.rs
+++ rustparallel/src/main.rs
@@ -1,7 +1,37 @@
 use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
-    let value = args.nth(1).unwrap();
-    let einheit = args.nth(0).unwrap();
-    println!("Hello, world!{}, {}",value,einheit);
+    let wert1 = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let wert2 = args.nth(0).unwrap();
+    let first_number =wert1.parse::<f32>().unwrap();
+    let second_number =wert2.parse::<f32>().unwrap();
+    let result = operate(operator,first_number,second_number);
+    println!("{}",output(first_number, operator, second_number, result));
+}
+fn operate(operator: char, first_number: f32, second_number: f32)->f32{
+    /*if operator == '+'{
+        first_number + second_number
+    }
+    else if operator == '*'{
+        first_number * second_number
+    }
+    else if operator == '-'{
+        first_number - second_number
+    }
+    else if operator == '/'{
+        first_number / second_number
+    }
+    else{0.0}*/
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
+        '/' => first_number / second_number,
+        _   => panic!("Invalid Operator used.")
+    }
+
+}
+fn output(first_number:f32,operator:char,second_number: f32,result:f32)->String{
+    format!("{} {} {} = {}", first_number,operator,second_number,result)
 }
